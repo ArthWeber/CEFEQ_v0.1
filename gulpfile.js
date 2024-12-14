7fc7fed05a52b4a7ce12fbf1efe506fdd528b0e5
Arthur Weber <darkw@PC-Weber>
2024-12-14 01:19:01 -0300

    imagemin added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-
-
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -9,7 +8,13 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/img/**/*', {encoding: false})
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles, images))
 }
